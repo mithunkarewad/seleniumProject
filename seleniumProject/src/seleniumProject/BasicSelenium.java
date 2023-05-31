ae06ea989c403d9c1e6c40762d5c92756a146d4a
mithun7720 <110145405+mithun7720@users.noreply.github.com>
2023-05-31 12:19:48 +0530

    WebDriver method

--- seleniumProject/src/seleniumProject/BasicSelenium.java
+++ seleniumProject/src/seleniumProject/BasicSelenium.java
@@ -6,8 +6,20 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class BasicSelenium {
 
 	public static void main(String[] args) {
-	WebDriver driver=new ChromeDriver();
-	driver.get("https://www.selenium.dev/downloads/");
+	ChromeDriver driver=new ChromeDriver();
+	//open browser
+	driver.get("https://www.facebook.com/");
+	driver.manage().window().maximize();
+	driver.manage().deleteAllCookies();
+	//check title
+	String title=driver.getTitle();
+	System.out.println(title);
+	//check url
+	String url =driver.getCurrentUrl();
+	System.out.println(url);
+	//check pagesource code
+	String html=driver.getPageSource();
+	System.out.println(html);
 
 	}
 
