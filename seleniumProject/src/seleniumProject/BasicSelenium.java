e1dd297404395ab286f65b78c7d50d8515bb56ae
mithun7720 <110145405+mithun7720@users.noreply.github.com>
2023-05-31 11:36:42 +0530

    rename class

--- seleniumProject/src/seleniumProject/BasicSelenium.java
+++ seleniumProject/src/seleniumProject/BasicSelenium.java
@@ -3,7 +3,7 @@ package seleniumProject;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class Basic {
+public class BasicSelenium {
 
 	public static void main(String[] args) {
 	WebDriver driver=new ChromeDriver();
